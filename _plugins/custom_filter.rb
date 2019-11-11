6b0b84f99f800862bf74dafacb52327610d704fe
jamesujeon <jamesujeon@gmail.com>
2019-11-11 18:35:32 +0900

    Add tags page with tag cloud

--- _plugins/custom_filter.rb
+++ _plugins/custom_filter.rb
@@ -4,6 +4,11 @@ module Jekyll::CustomFilter
             input = input[0..-11]
         end
     end
+
+    def sort_tags_by_post_count(tags)
+        max_post_count = tags.max_by { |k, v| v.size }[1].size
+        return tags.sort_by { |x| [max_post_count - x[1].size, x[0].downcase] }
+    end
 end
 
 Liquid::Template.register_filter(Jekyll::CustomFilter)
